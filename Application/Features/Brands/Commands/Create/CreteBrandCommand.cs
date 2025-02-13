8ab8a8f76bac1f2f0124ae1a5107d61e77e34682
Bugratoker33 <136968624+Bugratoker33@users.noreply.github.com>
2025-02-13 20:38:25 +0300

    SubClassıs IOC

--- Application/Features/Brands/Commands/Create/CreteBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreteBrandCommand.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
@@ -9,34 +10,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
-               //request                      //response
+//request                      //response
 public class CreteBrandCommand : IRequest<CretedBrandResponse>//bu işlem sonucunda döndüreceğimiz Responseyi döndür 
 {
     public string Name { get; set; } //requestir dışarıdan almak için
- 
-   
+
+
 
     public class CretedBrandResponseHandler : IRequestHandler<CreteBrandCommand, CretedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
         private readonly IMapper _mapper;
+        private readonly BrandBusinessRule _businessRule;
 
-        public CretedBrandResponseHandler(IBrandRepository brandRepository, IMapper mapper)
+        public CretedBrandResponseHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRule businessRule)
         {
             _brandRepository = brandRepository;
             _mapper = mapper;
+            _businessRule = businessRule;
         }
 
         public async Task<CretedBrandResponse>? Handle(CreteBrandCommand request, CancellationToken cancellationToken)
         {
+            await _businessRule.BrandNameCannotBeDuplicatedWhenInserted(request.Name);
             Brand brand = _mapper.Map<Brand>(request);//REQUESTEN GELEN NESNEYİ BRANDA ÇEVİR MAPPLE YANİ 
             brand.Id = Guid.NewGuid();
-          
-          
+
+
             var result = await _brandRepository.AddAsync(brand);
 
-           CretedBrandResponse cretedBrandResponse =_mapper.Map<CretedBrandResponse>(result);
-           
+            CretedBrandResponse cretedBrandResponse = _mapper.Map<CretedBrandResponse>(result);
+
             return cretedBrandResponse;
         }
     }
